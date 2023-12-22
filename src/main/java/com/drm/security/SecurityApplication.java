50c837eb0ef8e73a34002c76777f00f4c04210e9
prista <games.prista@mail.ru>
2023-12-22 16:11:35 +0300

    #Add Data source

--- src/main/java/com/drm/security/SecurityApplication.java
+++ src/main/java/com/drm/security/SecurityApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.drm.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SecurityApplication.class, args);
 	}
 
 }
--- src/test/java/com/drm/security/SecurityApplicationTests.java
+++ src/test/java/com/drm/security/SecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.drm.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
