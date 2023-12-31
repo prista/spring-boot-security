6f0d0893bcfb7dce25835574d1d9a04369ba7cbd
prista <games.prista@mail.ru>
2023-12-31 12:27:33 +0300

    #endpoints. Register new user

---
+++ src/main/java/com/drm/security/auth/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.drm.security.auth;
+
+import com.drm.security.config.JwtService;
+import com.drm.security.user.Role;
+import com.drm.security.user.User;
+import com.drm.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .firstname(registerRequest.getFirstname())
+                .lastname(registerRequest.getLastname())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authRequest) {
+        return null;
+    }
+}
